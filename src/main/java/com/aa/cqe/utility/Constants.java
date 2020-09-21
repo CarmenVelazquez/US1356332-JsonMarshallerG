da6984d7dcbabf359ef0086834e7c8dfac57b290
Durugkar <milind.durugkar@dxc.com>
2020-09-21 12:58:56 -0500

    delete class variable from constant

--- src/main/java/com/aa/cqe/utility/Constants.java
+++ src/main/java/com/aa/cqe/utility/Constants.java
@@ -14,5 +14,4 @@ public class Constants {
 	public static final String ERROR = "error";
 	public static final String UTC="UTC";
 	public static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
-	public static final String CLASS = "ClassName";
 }
