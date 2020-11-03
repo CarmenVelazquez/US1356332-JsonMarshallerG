05db7a2fa69c957d921ee66c04c3c4a3e06a0681
Durugkar <milind.durugkar@dxc.com>
2020-11-03 12:02:39 -0600

    Convert string to jsonString

--- src/main/java/com/aa/cqe/flightwrap/FlightWrapper.java
+++ src/main/java/com/aa/cqe/flightwrap/FlightWrapper.java
@@ -1,10 +1,16 @@
 package com.aa.cqe.flightwrap;
 
 import com.aa.cqe.flight.pojo.Flight;
+import com.google.gson.Gson;
 
 import lombok.Data;
 
 public @Data class FlightWrapper {
 	String cqeTrackingID;
 	Flight flight;
+	
+	@Override
+    public String toString() {
+    	return new Gson().toJson(this);
+    }
 }
